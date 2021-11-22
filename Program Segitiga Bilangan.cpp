78aa91d2e010f71903e360ac2247bf989ff479e9
Chandra Prasetya Putra <93764458+chandpras@users.noreply.github.com>
2021-11-22 08:54:31 +0700

    Add files via upload

---
+++ Program Segitiga Bilangan.cpp
@@ -0,0 +1,27 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    
+    int x;
+    cin>>x;
+    
+    if(x<=9&&x>=1){
+	
+    for(x; x>0; x--){
+    	for(int y=0; y<x; y++){
+    		cout<<x<<" ";
+		}
+		cout<<endl;
+	}
+}else{
+	cout<<"Bilangan tidak sesuai!";
+}
+    
+    return 0;
+}
