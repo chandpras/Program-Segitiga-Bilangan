f7dcd7479c7b4b6620f8770f130454636a01c0ed
Chandra Prasetya Putra <93764458+chandpras@users.noreply.github.com>
2022-03-05 18:22:21 +0700

    Update Program Segitiga Bilangan.cpp

--- Program Segitiga Bilangan.cpp
+++ Program Segitiga Bilangan.cpp
@@ -1,27 +1,24 @@
-#include <cmath>
-#include <cstdio>
-#include <vector>
 #include <iostream>
-#include <algorithm>
 using namespace std;
 
-
-int main() {
-    
+int main()
+{
     int x;
-    cin>>x;
+    cin >> x;
     
-    if(x<=9&&x>=1){
-	
-    for(x; x>0; x--){
-    	for(int y=0; y<x; y++){
-    		cout<<x<<" ";
+    if(x <= 9 && x >= 1)
+	{
+    for(x; x > 0; x--)
+    {
+    	for(int y = 0; y < x; y++)
+		{
+    		cout << x << " ";
 		}
-		cout<<endl;
-	}
-}else{
-	cout<<"Bilangan tidak sesuai!";
-}
-    
+		cout << endl;
+    }
+}else
+{
+	cout << "Bilangan tidak sesuai!";
+} 
     return 0;
 }
